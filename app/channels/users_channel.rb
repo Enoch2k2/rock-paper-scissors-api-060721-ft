ea9e86a863893b8959030b0a7d7a9e51ee8a551f
Enoch2k2 <enoch2k2@hotmail.com>
2021-10-29 14:10:06 -0500

    added users real time

--- app/channels/users_channel.rb
+++ app/channels/users_channel.rb
@@ -1,11 +1,21 @@
 class UsersChannel < ApplicationCable::Channel
   def subscribed
-    current_user.update(status: "online")
-    ActionCable.server.broadcast("users_channel", { type: "connecting" , users: User.logged_in_users })
     stream_from "users_channel"
   end
 
+  def receive(data)
+    user = User.find_by_id(data["user_id"])
+    user.update(status: "online")
+    ActionCable.server.broadcast("users_channel", { users: User.logged_in_users })
+  end
+
   def unsubscribed
-    current_user.update(status: "offline")
+  end
+  
+  def offline(data)
+    user = User.find_by_id(data["user_id"])
+    user.update(status: "offline")
+    ActionCable.server.broadcast("users_channel", { users: User.logged_in_users })
+    stop_stream_from "users_channel"
   end
 end
